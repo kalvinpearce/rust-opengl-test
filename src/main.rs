d4d0b4458ba8c59ee0d00aced2a0e3cd0f5d37ef
Kalvin Pearce <kalvinpearce@gmail.com>
2021-01-02 17:26:43 +1000

    Add sdl2 and barebones window creation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+extern crate sdl2;
+
 fn main() {
-    println!("Hello, world!");
+    let sdl = sdl2::init().unwrap();
+    let video_subsystem = sdl.video().unwrap();
+    let _window = video_subsystem
+        .window("OpenGL Test", 900, 700)
+        .resizable()
+        .build()
+        .unwrap();
+
+    let mut event_pump = sdl.event_pump().unwrap();
+    'main: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                sdl2::event::Event::Quit { .. } => break 'main,
+                _ => {}
+            }
+        }
+    }
 }
