76e616183e403fe55e2050e3ac6933e91d07969a
Kalvin Pearce <kalvinpearce@gmail.com>
2021-01-02 18:25:20 +1000

    Init OpenGL & add clear screen

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,22 @@
+extern crate gl;
 extern crate sdl2;
 
 fn main() {
     let sdl = sdl2::init().unwrap();
     let video_subsystem = sdl.video().unwrap();
-    let _window = video_subsystem
+    let window = video_subsystem
         .window("OpenGL Test", 900, 700)
+        .opengl()
         .resizable()
         .build()
         .unwrap();
+
+    let _gl_context = window.gl_create_context().unwrap();
+    let _gl = gl::load_with(|s| video_subsystem.gl_get_proc_address(s) as *const std::os::raw::c_void);
+
+    unsafe {
+        gl::ClearColor(0.3, 0.3, 0.5, 1.0);
+    }
 
     let mut event_pump = sdl.event_pump().unwrap();
     'main: loop {
@@ -17,5 +26,11 @@ fn main() {
                 _ => {}
             }
         }
+
+        unsafe {
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+        }
+
+        window.gl_swap_window();
     }
 }
