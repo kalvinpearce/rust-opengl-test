6901c87f5d2ec8004c5a93ca1546b3b4983ba559
Kalvin Pearce <kalvinpearce@gmail.com>
2021-01-02 19:18:01 +1000

    Add Shader struct

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 extern crate gl;
 extern crate sdl2;
 
+pub mod render_gl;
+
 fn main() {
     let sdl = sdl2::init().unwrap();
     let video_subsystem = sdl.video().unwrap();
+    let gl_attr = video_subsystem.gl_attr();
+    gl_attr.set_context_profile(sdl2::video::GLProfile::Core);
+    gl_attr.set_context_version(4, 5);
     let window = video_subsystem
         .window("OpenGL Test", 900, 700)
         .opengl()
@@ -12,9 +17,11 @@ fn main() {
         .unwrap();
 
     let _gl_context = window.gl_create_context().unwrap();
-    let _gl = gl::load_with(|s| video_subsystem.gl_get_proc_address(s) as *const std::os::raw::c_void);
+    let _gl =
+        gl::load_with(|s| video_subsystem.gl_get_proc_address(s) as *const std::os::raw::c_void);
 
     unsafe {
+        gl::Viewport(0, 0, 900, 700);
         gl::ClearColor(0.3, 0.3, 0.5, 1.0);
     }
 
