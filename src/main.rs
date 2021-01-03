5133f1dc6467ecb7fca7a08b748d16d3447e9d7d
Kalvin Pearce <kalvinpearce@gmail.com>
2021-01-04 00:08:49 +1000

    Split out debug submodule

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ extern crate failure;
 extern crate render_gl_derive;
 extern crate vec_2_10_10_10;
 
+mod debug;
 pub mod render_gl;
 pub mod resources;
 
@@ -26,7 +27,7 @@ struct Vertex {
 
 fn main() {
     if let Err(e) = run() {
-        println!("{}", failure_to_string(e));
+        println!("{}", debug::failure_to_string(e));
     }
 }
 
@@ -107,35 +108,4 @@ fn run() -> Result<(), failure::Error> {
     }
 
     Ok(())
-}
-
-pub fn failure_to_string(e: failure::Error) -> String {
-    use std::fmt::Write;
-
-    let mut result = String::new();
-
-    for (i, cause) in e
-        .iter_chain()
-        .collect::<Vec<_>>()
-        .into_iter()
-        .rev()
-        .enumerate()
-    {
-        if i > 0 {
-            let _ = writeln!(&mut result, "   Which caused the following issue:");
-        }
-        let _ = write!(&mut result, "{}", cause);
-        if let Some(backtrace) = cause.backtrace() {
-            let backtrace_str = format!("{}", backtrace);
-            if backtrace_str.len() > 0 {
-                let _ = writeln!(&mut result, " This happened at {}", backtrace);
-            } else {
-                let _ = writeln!(&mut result);
-            }
-        } else {
-            let _ = writeln!(&mut result);
-        }
-    }
-
-    result
 }
