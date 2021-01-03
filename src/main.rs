59392f5ec4f6b51c0e5ef3ebb6abad0a8725d6ea
Kalvin Pearce <kalvinpearce@gmail.com>
2021-01-03 23:22:35 +1000

    Add some OpenGL data types

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ extern crate sdl2;
 extern crate failure;
 #[macro_use]
 extern crate render_gl_derive;
+extern crate vec_2_10_10_10;
 
 pub mod render_gl;
 pub mod resources;
@@ -19,7 +20,7 @@ struct Vertex {
     #[location = 0]
     pos: data::f32_f32_f32,
     #[location = 1]
-    clr: data::f32_f32_f32,
+    clr: data::u2_u10_u10_u10_rev_float,
 }
 
 fn main() {
@@ -58,15 +59,15 @@ fn run() -> Result<(), failure::Error> {
     let vertices: Vec<Vertex> = vec![
         Vertex {
             pos: (0.5, -0.5, 0.0).into(),
-            clr: (1.0, 0.0, 0.0).into(),
+            clr: (1.0, 0.0, 0.0, 1.0).into(),
         },
         Vertex {
             pos: (-0.5, -0.5, 0.0).into(),
-            clr: (0.0, 1.0, 0.0).into(),
+            clr: (0.0, 1.0, 0.0, 1.0).into(),
         },
         Vertex {
             pos: (0.0, 0.5, 0.0).into(),
-            clr: (0.0, 0.0, 1.0).into(),
+            clr: (0.0, 0.0, 1.0, 1.0).into(),
         },
     ];
 
--- src/render_gl/data.rs
+++ src/render_gl/data.rs
@@ -3,6 +3,81 @@ use gl;
 #[allow(non_camel_case_types)]
 #[derive(Copy, Clone, Debug)]
 #[repr(C, packed)]
+pub struct i8_ {
+    pub d0: i8,
+}
+
+impl i8_ {
+    pub fn new(d0: i8) -> i8_ {
+        i8_ { d0 }
+    }
+
+    pub unsafe fn vertex_attrib_pointer(
+        gl: &gl::Gl,
+        stride: usize,
+        location: usize,
+        offset: usize,
+    ) {
+        gl.EnableVertexAttribArray(location as gl::types::GLuint);
+        gl.VertexAttribIPointer(
+            location as gl::types::GLuint,
+            1,
+            gl::BYTE,
+            stride as gl::types::GLint,
+            offset as *const gl::types::GLvoid,
+        );
+    }
+}
+
+impl From<i8> for i8_ {
+    fn from(other: i8) -> Self {
+        i8_::new(other)
+    }
+}
+
+// -----------------------------------------
+
+#[allow(non_camel_case_types)]
+#[derive(Copy, Clone, Debug)]
+#[repr(C, packed)]
+pub struct i8_float {
+    pub d0: i8,
+}
+
+impl i8_float {
+    pub fn new(d0: i8) -> i8_float {
+        i8_float { d0 }
+    }
+
+    pub unsafe fn vertex_attrib_pointer(
+        gl: &gl::Gl,
+        stride: usize,
+        location: usize,
+        offset: usize,
+    ) {
+        gl.EnableVertexAttribArray(location as gl::types::GLuint);
+        gl.VertexAttribPointer(
+            location as gl::types::GLuint,
+            1,
+            gl::BYTE,
+            gl::TRUE,
+            stride as gl::types::GLint,
+            offset as *const gl::types::GLvoid,
+        );
+    }
+}
+
+impl From<i8> for i8_float {
+    fn from(other: i8) -> Self {
+        i8_float::new(other)
+    }
+}
+
+// -----------------------------------------
+
+#[allow(non_camel_case_types)]
+#[derive(Copy, Clone, Debug)]
+#[repr(C, packed)]
 pub struct f32_f32_f32 {
     pub d0: f32,
     pub d1: f32,
@@ -35,5 +110,41 @@ impl f32_f32_f32 {
 impl From<(f32, f32, f32)> for f32_f32_f32 {
     fn from(other: (f32, f32, f32)) -> Self {
         f32_f32_f32::new(other.0, other.1, other.2)
+    }
+}
+
+// -----------------------------------------
+
+#[allow(non_camel_case_types)]
+#[derive(Copy, Clone, Debug)]
+#[repr(C, packed)]
+pub struct u2_u10_u10_u10_rev_float {
+    pub inner: ::vec_2_10_10_10::Vector,
+}
+
+impl u2_u10_u10_u10_rev_float {
+    pub unsafe fn vertex_attrib_pointer(
+        gl: &gl::Gl,
+        stride: usize,
+        location: usize,
+        offset: usize,
+    ) {
+        gl.EnableVertexAttribArray(location as gl::types::GLuint);
+        gl.VertexAttribPointer(
+            location as gl::types::GLuint,
+            4,
+            gl::UNSIGNED_INT_2_10_10_10_REV,
+            gl::TRUE,
+            stride as gl::types::GLint,
+            offset as *const gl::types::GLvoid,
+        );
+    }
+}
+
+impl From<(f32, f32, f32, f32)> for u2_u10_u10_u10_rev_float {
+    fn from(other: (f32, f32, f32, f32)) -> Self {
+        u2_u10_u10_u10_rev_float {
+            inner: ::vec_2_10_10_10::Vector::new(other.0, other.1, other.2, other.3),
+        }
     }
 }
