23455f0d4891f937f480e454a69c3ee18f487d63
Kalvin Pearce <kalvinpearce@gmail.com>
2021-01-03 19:58:24 +1000

    Add better formatting to debugs using failure

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,26 @@
 extern crate gl;
 extern crate sdl2;
+#[macro_use]
+extern crate failure;
 
 pub mod render_gl;
 pub mod resources;
 
+use failure::err_msg;
 use resources::Resources;
 use std::path::Path;
 
 fn main() {
+    if let Err(e) = run() {
+        println!("{}", failure_to_string(e));
+    }
+}
+
+fn run() -> Result<(), failure::Error> {
     let res = Resources::from_relative_exe_path(Path::new("assets")).unwrap();
 
-    let sdl = sdl2::init().unwrap();
-    let video_subsystem = sdl.video().unwrap();
+    let sdl = sdl2::init().map_err(err_msg)?;
+    let video_subsystem = sdl.video().map_err(err_msg)?;
     let gl_attr = video_subsystem.gl_attr();
     gl_attr.set_context_profile(sdl2::video::GLProfile::Core);
     gl_attr.set_context_version(4, 5);
@@ -22,7 +31,7 @@ fn main() {
         .build()
         .unwrap();
 
-    let _gl_context = window.gl_create_context().unwrap();
+    let _gl_context = window.gl_create_context().map_err(err_msg)?;
     let gl = gl::Gl::load_with(|s| {
         video_subsystem.gl_get_proc_address(s) as *const std::os::raw::c_void
     });
@@ -89,7 +98,7 @@ fn main() {
         gl.BindVertexArray(0);
     }
 
-    let mut event_pump = sdl.event_pump().unwrap();
+    let mut event_pump = sdl.event_pump().map_err(err_msg)?;
     'main: loop {
         for event in event_pump.poll_iter() {
             match event {
@@ -110,4 +119,37 @@ fn main() {
 
         window.gl_swap_window();
     }
+
+    Ok(())
+}
+
+pub fn failure_to_string(e: failure::Error) -> String {
+    use std::fmt::Write;
+
+    let mut result = String::new();
+
+    for (i, cause) in e
+        .iter_chain()
+        .collect::<Vec<_>>()
+        .into_iter()
+        .rev()
+        .enumerate()
+    {
+        if i > 0 {
+            let _ = writeln!(&mut result, "   Which caused the following issue:");
+        }
+        let _ = write!(&mut result, "{}", cause);
+        if let Some(backtrace) = cause.backtrace() {
+            let backtrace_str = format!("{}", backtrace);
+            if backtrace_str.len() > 0 {
+                let _ = writeln!(&mut result, " This happened at {}", backtrace);
+            } else {
+                let _ = writeln!(&mut result);
+            }
+        } else {
+            let _ = writeln!(&mut result);
+        }
+    }
+
+    result
 }
--- src/render_gl.rs
+++ src/render_gl.rs
@@ -3,23 +3,20 @@ use gl;
 use std;
 use std::ffi::{CStr, CString};
 
-#[derive(Debug)]
+#[derive(Debug, Fail)]
 pub enum Error {
+    #[fail(display = "Failed to load resource {}", name)]
     ResourceLoad {
         name: String,
+        #[cause]
         inner: resources::Error,
     },
-    CanNotDetermineShaderTypeForResource {
-        name: String,
-    },
-    CompileError {
-        name: String,
-        message: String,
-    },
-    LinkError {
-        name: String,
-        message: String,
-    },
+    #[fail(display = "Can not determine shader type for resource {}", name)]
+    CanNotDetermineShaderTypeForResource { name: String },
+    #[fail(display = "Failed to compile shader {}: {}", name, message)]
+    CompileError { name: String, message: String },
+    #[fail(display = "Failed to link program {}: {}", name, message)]
+    LinkError { name: String, message: String },
 }
 
 /* Program */
